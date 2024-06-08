6a1baeed87ccf1b3a194c1235747e303617aba27
Widson Martins <131882974+widson27@users.noreply.github.com>
2024-06-08 11:27:20 -0300

    Add files via upload

---
+++ ator.js
@@ -0,0 +1,60 @@
+let xAtor = 100;
+let yAtor = 366;
+let colisao = false;
+let meusPontos = 0;
+
+function mostraAtor(){
+  image(imagemDoAtor, xAtor, yAtor, 30, 30);
+}
+
+function movimentaAtor(){
+  if(keyIsDown(UP_ARROW)) {
+    yAtor -= 3;
+  }
+  if(keyIsDown(DOWN_ARROW)) {
+    if(podeSeMover()){
+      yAtor += 3;
+    }
+  }
+}
+
+function verificaColisao(){
+  for (i = 0; i < imagemCarros.length; i = i + 1){
+    colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15);
+    if (colisao){
+      voltaAtorParaPosiçãoInicial();
+      somDaColisao.play();
+      xCarros[i] = 600;
+    if (pontosMaiorQueZero()) {
+      meusPontos -= 1;
+     }  
+    }
+  } 
+}
+
+function voltaAtorParaPosiçãoInicial() {
+  yAtor = 366;
+}
+
+function incluiPontos(){
+  textAlign(CENTER);
+  textSize(25);
+  fill(color(255,240, 60));
+  text(meusPontos,width / 5, 27)
+}
+
+function marcaPonto(){
+  if (yAtor < 15){
+    meusPontos += 1;
+    somDoPonto.play();
+    voltaAtorParaPosiçãoInicial();
+  }
+}
+
+function pontosMaiorQueZero(){
+  return meusPontos > 0;
+}
+
+function podeSeMover() {
+  return yAtor < 366;
+}
